6fcc069bbe0b2b4464acc880e89ec2f73fe84c84
Vasiliy <vapalados@gmail.com>
2022-09-15 21:54:20 +0700

    ver6

---
+++ 006_Base/Program.cs
@@ -0,0 +1,17 @@
+﻿int a = 1;
+int b = 2;
+int c = 5;
+int d = 99;
+int e = 44;
+
+int max = a;
+
+if (a > max ) max = a;
+if (b > max ) max = b;
+if (c > max ) max = c;
+if (d > max ) max = d;
+if (e > max ) max = e;
+
+Console.Write("max = ");
+Console.WriteLine(max);
+
