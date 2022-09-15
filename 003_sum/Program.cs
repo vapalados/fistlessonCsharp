3cac924ce29c6d228c8ee783ad433e787727c91c
Vasiliy <vapalados@gmail.com>
2022-09-15 19:56:00 +0700

    Random

--- 003_sum/Program.cs
+++ 003_sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 13;
-int numberB = 5;
+﻿int numberA = new Random().Next(1, 10);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.WriteLine(result);
